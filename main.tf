8395017e87e1edc46d73a1f9fa9a424de7c52266
mz19933 <mz1993@gitlab.com>
2024-04-09 00:14:27 +0300

    add steps to main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,52 @@ provider "aws" {
   profile = "default"
 }
 
+# VPC
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags ={
+    Name = "production"
+  }
+}
+
+# Internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+
+# Custom route table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    # Defualt route, send all tarfic
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    # Same for IPv6, send all tarfic
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+}
+
+# Subnet
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1"
+
+  tags = {
+    Name = "Prod-subnet"
+  }
+}
+
 # Include the self-made available_regions module
 module "available_regions" {
   source = "./available_regions"
